defe57f0c596ffa7fefc83b5bdd23c217c84c74f
konrad <konje@github.com>
2019-11-12 11:32:43 +0000

    new repositiory

---
+++ ThrustAllocation.cpp
@@ -0,0 +1,47 @@
+/*
+ThrustAllocation.h - library for allocating (distributing)
+pwm signal to four Mallard's thrusters. The input is velocity vector
+[x_dot y_dot  psi_dot].  Output is pwm1 to pwm4.
+Created by Konrad Gornicki, September 27, 2019.
+*/
+
+#include "Arduino.h"
+#include "ThrustAllocation.h"
+#include <Servo.h>
+#include "geometry_msgs/Twist.h"
+
+// ----- Constructor -----
+ThrustAllocation::ThrustAllocation(int pin_1, int pin_2, int pin_3, int pin_4)
+{   
+    // Assign output pins to thrusters:
+    _thruster_1.attach(pin_1);
+    _thruster_2.attach(pin_2);
+    _thruster_3.attach(pin_3);
+    _thruster_4.attach(pin_4);
+    //Initialize each thruster with pwm value (no rotation = 1500):
+    _thruster_1.writeMicroseconds(1500);
+    _thruster_2.writeMicroseconds(1500);
+    _thruster_3.writeMicroseconds(1500);
+    _thruster_4.writeMicroseconds(1500);
+}
+
+// ----- Functions -----
+double ThrustAllocation::output_pwm(float x_body_input, float y_body_input, float psi_body_input)
+{   
+    //Takes /joy commands and transform them to pwm.
+    _x = (x_body_input)*_linear_scale;
+    _y = (y_body_input)*_linear_scale;
+    _psi = (-psi_body_input)*_angular_scale;
+
+    _pwm1 = 1500 + 0.5*_x + _a*_psi;
+    _pwm2 = 1500 + 0.5*_x - _a*_psi;
+    _pwm3 = 1500 - 0.5*_y + _b*_psi;
+    _pwm4 = 1500 - 0.5*_y - _b*_psi;
+
+    _thruster_1.writeMicroseconds(_pwm1);
+    _thruster_2.writeMicroseconds(_pwm2);
+    _thruster_3.writeMicroseconds(_pwm3);
+    _thruster_4.writeMicroseconds(_pwm4);
+    
+    return _pwm1; //for testing send to publisher()
+}
---
+++ ThrustAllocation.h
@@ -0,0 +1,28 @@
+/*
+ThrustAllocation.h - library for allocating (distributing)
+pwm signal to four Mallard's thrusters. The input is velocity vector
+[x_dot y_dot  psi_dot]. Output is pwm1 to pwm4.
+Created by Konrad Gornicki, September 27, 2019.
+*/
+
+//Construct - prevents including the library twice:
+#ifndef ThrustAllocation_h
+#define ThrustAllocation_h
+
+#include "Arduino.h"
+#include <Servo.h>
+#include "geometry_msgs/Twist.h"
+
+class ThrustAllocation
+{
+ public:
+   ThrustAllocation(int pin_1, int pin_2, int pin_3, int pin_4);
+   double output_pwm(float x_body_input, float y_body_input, float psi_body_input);
+ private:
+   Servo _thruster_1,_thruster_2,_thruster_3,_thruster_4;
+   double _x=0,_y=0,_psi=0,_pwm1=0, _pwm2=0,_pwm3=0,_pwm4=0;
+   double _linear_scale=400, _angular_scale=100;
+   double _a=1.0556,_b=1.1955;
+};
+
+#endif
