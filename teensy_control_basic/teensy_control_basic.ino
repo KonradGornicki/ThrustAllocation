8bd33fd1ef9fac426a9950b523590d1c20b99210
konrad <konradgornicki@github.com>
2020-11-10 11:03:05 +0000

    added teensy_control_basic.ino as an example usage

---
+++ teensy_control_basic/teensy_control_basic.ino
@@ -0,0 +1,67 @@
+
+#include <ros.h>
+#include <geometry_msgs/Twist.h>
+#include <std_msgs/Float64.h>
+#include <ThrustAllocationROS.h>
+
+// ----- Variables -----
+double pwm_out = 0; //return values for ThrustAllocation library
+int state = 0; // to toggle LED
+const int LED = 13;
+std_msgs::Float64 test; // publisher's variables for testting
+
+
+// ----- ROS and Calback declarations ------
+void callback(const geometry_msgs::Twist&);
+ros::NodeHandle nh;
+ros::Subscriber<geometry_msgs::Twist> sub_twist("/mallard/thruster_command", &callback);
+// ros::Publisher pub1("teensy_test_topic",&test); //for testing prurposes
+
+
+// ----- Constructor ----- 
+// Thruster initialization: Servo.attach() and Servo.writeMiliseconds(initial_value) 
+// Each thruster gets its pin assignment (pin numbers) and initial pwm value. Must be outside of setup().
+ThrustAllocationROS allocate(6,8,10,12); 
+
+// ----- Setup -----
+void setup()
+{
+  nh.initNode();
+    
+  //start subscriber and listen to incomming messages
+  nh.subscribe(sub_twist); //if message present init callback and pass Twist message
+  //adveriste on topic for testing
+//   nh.advertise(pub1);
+
+  // show output on led
+  pinMode(LED,OUTPUT);
+  digitalWrite(LED,HIGH);
+  delay(1000);
+}
+
+// ----- Calback definition -----
+void callback(const geometry_msgs::Twist& twist_msg)
+{  
+
+    u = twist_msg.linear.x;
+    v = twist_msg.linear.y;
+    r = twist_msg.angular.z;  
+
+    pwm_out = allocate.output_pwm(u,v,r);
+    // pwm_out = allocate.output_pwm(twist_msg);
+ 
+    //Publish...
+    // test.data = pwm_out;
+    // pub1.publish(&test);
+    
+    //... and blink LED
+    // digitalWrite(LED, (state) ? HIGH:LOW);
+    // state = !state;
+}
+
+// ----- Main Loop ------
+void loop()
+{
+  nh.spinOnce();
+  delay(50); //miliseconds
+}
